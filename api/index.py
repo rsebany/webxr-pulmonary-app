aef8814405c605acc16092977a50039e76000518
Romualdo <romualdosebany@gmail.com>
2025-12-05 19:19:50 +0100

    update

--- api/index.py
+++ api/index.py
@@ -14,5 +14,10 @@ from fastapi_app import app
 from mangum import Mangum
 
 # Wrapper Mangum pour adapter FastAPI à AWS Lambda/Vercel
+# Vercel détecte automatiquement cette variable 'handler'
 handler = Mangum(app, lifespan="off")
+
+# Alternative: exporter aussi 'app' directement si nécessaire
+# Vercel peut utiliser soit 'handler' soit 'app'
+__all__ = ["handler", "app"]
 
